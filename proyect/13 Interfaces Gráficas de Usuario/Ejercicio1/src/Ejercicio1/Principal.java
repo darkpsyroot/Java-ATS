199f81dae81c74902bcafb3652be14c3c571908f
Francisco Reyes <franciscoalonso.reyes94@gmail.com>
2023-03-04 20:30:46 -0600

    Resolucion de conflictos

--- proyect/13 Interfaces Gráficas de Usuario/Ejercicio1/src/Ejercicio1/Principal.java
+++
@@ -1,9 +0,0 @@
-
-package Ejercicio1;
-
-public class Principal {
-    public static void main(String[] args) {
-        Ventana ventana = new Ventana();
-        ventana.setVisible(true); //hacer visible la ventana
-    }
-}
--- proyect/13 Interfaces Gráficas de Usuario/Ejercicio2/src/Codigo/Principal.java
+++
@@ -1,9 +0,0 @@
-
-package Codigo;
-
-public class Principal {
-    public static void main(String[] args) {
-        Ventana ventana = new Ventana();
-        ventana.setVisible(true);
-    }
-}
